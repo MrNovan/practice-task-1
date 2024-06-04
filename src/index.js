e80c94abbc53ed29908007bbd448d1e8ea2eed2e
MrNovan <toni.justdoit@bk.ru>
2024-06-04 11:45:04 +0300

    add validate form

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ export default () => {
   const nameInput = document.querySelector('#name');
   const surnameInput = document.querySelector('#surname');
   const emailInput = document.querySelector('#email');
+  const phoneInput = document.querySelector('#phone');
+  const aboutInput = document.querySelector('#about');
 
   nameInput.addEventListener('input', () => {
     const nameRegex = /^[a-zA-Zа-яА-Я]+$/;
@@ -26,7 +28,6 @@ export default () => {
   });
 
   emailInput.addEventListener('input', () => {
-    console.log(emailInput.value);
     const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
     if (emailRegex.test(emailInput.value)) {
       emailInput.classList.remove('input-invalid');
@@ -34,6 +35,28 @@ export default () => {
     } else {
       emailInput.classList.remove('input-valid');
       emailInput.classList.add('input-invalid');
+    }
+  });
+
+  phoneInput.addEventListener('input', () => {
+    const phoneRegex = /^\+\d{10}$/;
+    if (phoneRegex.test(phoneInput.value)) {
+      phoneInput.classList.remove('input-invalid');
+      phoneInput.classList.add('input-valid');
+    } else {
+      phoneInput.classList.remove('input-valid');
+      phoneInput.classList.add('input-invalid');
+    }
+  });
+
+  aboutInput.addEventListener('input', () => {
+    const aboutRegex = /^[a-zA-ZА-Яа-я0-9\s\\.,\-']{1,50}$/;
+    if (aboutRegex.test(aboutInput.value)) {
+      aboutInput.classList.remove('input-invalid');
+      aboutInput.classList.add('input-valid');
+    } else {
+      aboutInput.classList.remove('input-valid');
+      aboutInput.classList.add('input-invalid');
     }
   });
 };
